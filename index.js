732d7200ea00dc502227388442c1d25e8267ddbd
Terry Brown <tabrownj@gmail.com>
2021-03-11 12:43:30 -0500

    WIP

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log( 'web39 rules')
+require('dotenv').config();
+
+const express = require('express');
+
+const server = express();
+
+server.use(express.json())
+console.log(process.env.NODE_ENV);
+if( process.env.NODE_ENV !== 'production' ){ // on heroku, NODE_ENV has value of production
+    const cors = require('cors');
+    server.use(cors())
+}
