2e7a4f78c69283ac23409f6d6261c926d164d5e4
Terry Brown <tabrownj@gmail.com>
2021-03-11 13:57:32 -0500

    added api

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 4000;
 
 server.get('/api/hello', ( req, res ) => {
     res.json({
-        message: 'hello'
+        message: 'hello all of you'
     })
 })
 server.get( '*', ( req, res ) => {
