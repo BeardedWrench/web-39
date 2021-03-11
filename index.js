572326fb9b186e821df7ab97a25bc7f55d7688d5
Terry Brown <tabrownj@gmail.com>
2021-03-11 12:55:24 -0500

    WIP

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ const express = require('express');
 const server = express();
 
 server.use(express.json())
-console.log(process.env.NODE_ENV);
+
 if( process.env.NODE_ENV !== 'production' ){ // on heroku, NODE_ENV has value of production
     const cors = require('cors');
     server.use(cors())
 }
+const PORT = process.env.PORT || 4000;
+
+server.use( '*', ( req, res ) => {
+    res.status(200).json({
+        message: 'Api is running'
+    })
+})
+
+server.listen( PORT, () => {
+    console.log("Environment: ", process.env.NODE_ENV)
+    console.log(`listening on port ${PORT}`)
+})
